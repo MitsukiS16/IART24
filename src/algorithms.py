1b1d784480e59bfc715b43861330486130d71430
Clarisse Carvalho <up202008444@edu.fe.up.pt>
2024-02-26 00:41:32 +0000

    Add algorithms suggestions

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,4 +1,25 @@
 # Algorithm Functions
+
+# Sugestoes de algoritmos para implementar: (5 primeiros +importantes, os outros fazemos dependendo do tempo que temos :P)
+
+# Gready 
+# Random Neighbour
+# [Metaheuristic Methods] Genetic
+# [Metaheuristic Methods] Simulated Annealing
+# [Metaheuristic Methods] Particle Swarm Optimization
+# [Iterative Methods] Gradient Descent
+# [Iterative Methods] Newton's Method
+# [Iterative Methods] Conjugate Gradient Method
+# [Analytical Methods] Calculus-based Optimization
+# [Analytical Methods] Linear Programming
+# [Derivative-Free Optimization] Pattern Search
+# [Derivative-Free Optimization] Nelder-Mead Method
+# [Derivative-Free Optimization] Evolutionary Strategies
+
+# Escolher um algoritmos, criar o codigo e ver score :) simples :P
+
+# Falta acrescentar o trabalho do valter aqui nestas funcoes :)) por isso qnd tiver tempo primeiro faça isso , antes de começar a criar os algoritmos, para nao haver codigo repetido, obrigadaa (assi: clarisse)
+
 def algorithm1(file):
     print(f"Algorithm 1 selected for file: {file}")
 
