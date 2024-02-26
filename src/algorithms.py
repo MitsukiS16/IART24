04ebe3d7c21ea0a9804e36056ac79c919c01a37a
Clarisse Carvalho <up202008444@edu.fe.up.pt>
2024-02-26 00:28:44 +0000

    Add ini algorithms menu

---
+++ src/algorithms.py
@@ -0,0 +1,9 @@
+# Algorithm Functions
+def algorithm1(file):
+    print(f"Algorithm 1 selected for file: {file}")
+
+def algorithm2(file):
+    print(f"Algorithm 2 selected for file: {file}")
+
+def algorithm3(file):
+    print(f"Algorithm 3 selected for file: {file}")
--- src/menu.py
+++ src/menu.py
@@ -1,125 +1,117 @@
 import sys
 import os
 
+from algorithms import algorithm1
+from algorithms import algorithm2
+from algorithms import algorithm3
+
+
+
 # Exit Menu Function
 def exit_application():
     print("Exiting the application. Goodbye!")
     sys.exit()
 
+
+
 # Function Test Yet to be done 
 def idkyet():
     print("Not yet implemented")
 
-# Choose Library Menu Function
-def choose_library_menu():
+
+
+# Book Scanning Menu Function
+def book_scanning_menu():
     os.system('clear')
 
-    print("----------------------------------------------------")
-    print("| Library File Menu                                |")
-    print("----------------------------------------------------")
-    print("| 1. a_example                                     |")
-    print("| 2. b_read_on                                     |")
-    print("| 3. c_incunabula                                  |")
-    print("| 4. d_tough_choices                               |")
-    print("| 5. e_so_many_books                               |")
-    print("| 6. f_libraries_of_the_world                      |")
-    print("| 0. Main Menu                                     |")
-    print("----------------------------------------------------")
-    
-    choice = input("Please enter your choice: ")
+    print("-------------------------------------------------------------")
+    print("| Book Scanning Menu                                        |")
+    print("-------------------------------------------------------------")
+    print("| Please select an input file :                             |")
+    print("| 1. a_example                                              |")
+    print("| 2. b_read_on                                              |")
+    print("| 3. c_incunabula                                           |")
+    print("| 4. d_tough_choices                                        |")
+    print("| 5. e_so_many_books                                        |")
+    print("| 6. f_libraries_of_the_world                               |")
+    print("| 0. Main Menu                                              |")
+    print("-------------------------------------------------------------")
+    inputfile = input("Please enter your choice: ")
 
-    options = {
-        '1': choose_library_menu,
-        '2': choose_library_menu,  # Change to correct function name or idkyet
-        '0': menu  # Change to correct function name
-    }
+    if inputfile in ['1', '2', '3', '4', '5', '6']:
+        print("-------------------------------------------------------------")
+        print("| Please select the algorithm:                              |")
+        print("| 1. Algorithm 1                                            |")
+        print("| 2. Algorithm 2                                            |")
+        print("| 3. Algorithm 3                                            |")
+        print("| 0. Main Menu                                              |")
+        print("-------------------------------------------------------------")
+        inputalgorithm = input("Please enter your choice: ")
 
-    selected_option = options.get(choice)
+        options = {
+            '1': algorithm1,
+            '2': algorithm2,
+            '3': algorithm3,
+            '0': menu
+        }
 
-    if selected_option:
-        selected_option()  # Call the selected function
+        selected_algorithm = options.get(inputalgorithm)
+
+        if selected_algorithm:
+            selected_algorithm(inputfile)
+        else:
+            print("Invalid choice. Please enter a valid option.")
+            best_scanning_menu()
+    elif inputfile == '0':
+        menu()
     else:
         print("Invalid choice. Please enter a valid option.")
-        choose_library_menu()
+        best_scanning_menu()
 
-# Choose Algorithm Menu Function
-def choose_algorithm_menu():
-    os.system('clear')
 
-    print("----------------------------------------------------")
-    print("| Library File Menu                                |")
-    print("----------------------------------------------------")
-    print("| 1. Algorithm 1                                   |")
-    print("| 2. Algorithm 2                                   |")
-    print("| 3. Algorithm 3                                   |")
-    print("| 0. Main Menu                                     |")
-    print("----------------------------------------------------")
 
-    choice = input("Please enter your choice: ")
-
-    options = {
-        '1': idkyet,
-        '2': idkyet,
-        '3': idkyet,
-        '0': menu
-    }
-
-    selected_option = options.get(choice)
-
-    if selected_option:
-        selected_option()
-    else:
-        print("Invalid choice. Please enter a valid option.")
-        choose_algorithm_menu()
+        
 
 # Best Score Menu Function
 def best_score_menu():
     os.system('clear')
 
-    print("----------------------------------------------------")
-    print("| Best Score Menu                                  |")
-    print("----------------------------------------------------")
-    print("| 1. ...                                           |")
-    print("| 2. ...                                           |")
-    print("| 3. ...                                           |")
-    print("| 0. Main Menu                                     |")
-    print("----------------------------------------------------")
-    choice = input("Please enter your choice: ")
+    print("-------------------------------------------------------------")
+    print("| Best Score Menu                                           |")
+    print("-------------------------------------------------------------")
+    print("| File                      | Score       | Algorithm       |")
+    print("| a_example                 | 000 000 000 | -               |")
+    print("| b_read_on                 | 000 000 000 | -               |")
+    print("| c_incunabula              | 000 000 000 | -               |")
+    print("| d_tough_choices           | 000 000 000 | -               |")
+    print("| e_so_many_books           | 000 000 000 | -               |")
+    print("| f_libraries_of_the_world  | 000 000 000 | -               |")
+    print("-------------------------------------------------------------")
 
-    options = {
-        '1': idkyet,
-        '2': idkyet,
-        '3': idkyet,
-        '0': menu
-    }
-
-    selected_option = options.get(choice)
-
-    if selected_option:
-        selected_option()
+    choice = input("Press 0 to go back to main menu.\n")
+    if choice == '0':
+        menu()
     else:
         print("Invalid choice. Please enter a valid option.")
         best_score_menu()
-
-
 
 # Main menu Function
 def menu():
     os.system('clear')
 
-    print("----------------------------------------------------")
-    print("| Welcome to our application                       |")
-    print("----------------------------------------------------")
-    print("| What do you want to do:                          |")
-    print("| 1. Choose a library file                         |")
-    print("| 2. See the best score for each library           |")
-    print("| 0. Exit                                          |")
-    print("----------------------------------------------------")
+    print("-------------------------------------------------------------")
+    print("| Welcome to our application                                |")
+    print("-------------------------------------------------------------")
+    print("| What do you want to do:                                   |")
+    print("| 1. Book Scanning                                          |")
+    print("| 2. See the best score for each library                    |")
+    print("| 0. Exit                                                   |")
+    print("-------------------------------------------------------------")
     
     choice = input("Please enter your choice: ")
 
     options = {
-        '1': choose_library_menu,
+        '1': book_scanning_menu,
         '2': best_score_menu,
         '0': exit_application
     }
