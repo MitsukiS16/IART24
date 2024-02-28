0a1dc58c4544810244a9e52e8b911fc81d3b3868
Clarisse Carvalho <up202008444@edu.fe.up.pt>
2024-02-28 11:17:40 +0000

    Remove not working stuff

--- src/algorithms.py
+++ src/algorithms.py
@@ -2,8 +2,12 @@
 
 # Sugestoes de algoritmos para implementar: (5 primeiros +importantes, os outros fazemos dependendo do tempo que temos :P)
 
-# Gready 
-# Random Neighbour
+### Initial Solutions
+# Random Solution
+# Trivial Solution
+# Greedy Constrution
+
+### Algorithms to implement
 # [Metaheuristic Methods] Genetic
 # [Metaheuristic Methods] Simulated Annealing
 # [Metaheuristic Methods] Particle Swarm Optimization
@@ -18,6 +22,9 @@
 
 # Escolher um algoritmos, criar o codigo e ver score :) simples :P
 
+
+
+
 ########################################
 # Imports 
 from library import Library
@@ -30,128 +37,48 @@ import copy
 libraries_shipped = set()
 libraries = {}
 scores = []
-
+i = 0
+books = {}
+libID = 0
+islib = False
+diffbooks = None
+numLibs = None
+shipping_days = None
 
 ########################################
 # Main and Auxiliar Functions for Optimization Algorithms
 
-# Algorithm 1
-def algorithm1(file):
-    alg_name = "*name alg 1*"
-    solution = generate_solution()
-    final_score = evaluate_solution(solution)
-    print_info(alg_name, file, final_score)
 
 
 # Helper function to print algorithm information
-def print_info(alg_name, input_file):
+def print_info(alg_name,file_path,init_solution,score):
     print("-------------------------------------------------------------")
-    print(f"| Algorithm {alg_name} selected for file {input_file}:")
-    print("| The total score was: {score}")
+    print(f"| Algorithm: {alg_name}")
+    print(f"| File: {file_path}")
+    print(f"| Initial Solution: {init_solution}")
+    print(f"| The final score was: {score}")
     print("-------------------------------------------------------------")
 
 
-# Random sign up function
-def random_sign_up():
-    return np.random.randint(0, numLibs)
 
-
-# Function to get book scores
-def get_book_scores(shipped_books):
-    book_scores = []
-    for book in shipped_books:
-        book_scores.append(scores[int(book)])
-    return book_scores
-
-
-# Neighbor solution function
-def neighbor_solution(solution, libraries_shipped):
-    book = np.random.choice(list(solution))
-    sol_len = len(solution)
-    solution.remove(book)
-    for library in libraries_shipped:
-        keys = list(libraries[library].books.keys())
-        if int(book) in keys:
-            available_books = [b for b in keys if b != int(book) and str(b) not in solution]
-            if available_books:
-                new_book = np.random.choice(available_books)
-                solution.add(str(new_book))
-                break
-    if sol_len > len(solution):
-        solution.add(book)
-    return solution
-
-
-# Generate random solution function
-def generate_random_solution(shipping_days, numLibs, diffbooks, libraries_shipped):
-    visited_libs = set()
-    canShip_libs = set()
-    shipped_books = set()
-    randlibID = random_sign_up()
-    visited_libs.add(randlibID)
-    canShip_libs.add(randlibID)
-    shipping_days -= libraries[randlibID].sign_up_time
-    randlibSignUp = 0
-    while shipping_days > 0:
-        if randlibSignUp == 0:
-            if len(visited_libs) < numLibs:
-                while True:
-                    randlibID = random_sign_up()
-                    if randlibID not in visited_libs:
-                        visited_libs.add(randlibID)
-                        randlibSignUp = libraries[randlibID].sign_up_time
-                        break
-            if randlibID not in canShip_libs:
-                canShip_libs.add(randlibID)
-        for libid in canShip_libs:
-            sameDayShipping = libraries[libid].shipping_time
-            book_keys_list = list(libraries[libid].books.keys())
-            if not book_keys_list:
-                continue
-            else:
-                while sameDayShipping > 0:
-                    if diffbooks == len(shipped_books):
-                        libraries_shipped = copy.deepcopy(visited_libs)
-                        return shipped_books, libraries_shipped
-                    else:
-                        random_key = np.random.choice(book_keys_list)
-                        if random_key not in shipped_books:
-                            shipped_books.add(random_key)
-                            sameDayShipping -= 1
-        randlibSignUp -= 1
-        shipping_days -= 1
-    libraries_shipped = copy.deepcopy(visited_libs)
-    return shipped_books, libraries_shipped
-
-    
-# Simulated Annealing solution function
-def get_sa_solution(num_iterations, shipping_days, numLibs, diffbooks, libraries_shipped):
-    iteration = 0
-    temperature = 1000
-    cooling_rate = 0.999
-    solution, libraries_shipped = generate_random_solution(shipping_days, numLibs, diffbooks, libraries_shipped)
-    score = evaluate_solution(solution)
-    best_solution = copy.deepcopy(solution)
-    best_score = score
-    while iteration < num_iterations:
-        temperature *= cooling_rate
-        iteration += 1
-        neighbor = neighbor_solution(best_solution, libraries_shipped)
-        neighbor_score = evaluate_solution(neighbor)
-        eval = neighbor_score - best_score
-        if eval > 0:
-            best_solution = neighbor
-            best_score = neighbor_score
-        elif np.exp(eval/temperature) >= np.random.rand():
-            best_solution = neighbor
-            best_score = neighbor_score
-    return best_solution
-
-
-# Function to evaluate solution
-def evaluate_solution(solution):
+# Algorithm 3
+def algorithm3(file_path,init_solution):
+    alg_name = "Algorithmo 3 :)"
     score = 0
-    for arg in solution:
-        score += int(arg)
-    return score
+    print_info(alg_name,file_path,init_solution,score)
+    return 0
+
+# Algorithm 2
+def algorithm2(file_path,init_solution):
+    alg_name = "Algorithmo 3 :)"
+    score = 0
+    print_info(alg_name,file_path,init_solution,score)
+    return 0
+
+# Algorithm 1
+def algorithm1(file_path,init_solution):
+    alg_name = "Algorithmo 1 :)"
+    score = 0
+    print_info(alg_name,file_path,init_solution,score)
+    return 0
 
