63b15e245df3ba920f27dac5bbbc3a5a2fac8567
Clarisse Carvalho <up202008444@fe.up.pt>
2024-03-11 20:06:51 +0000

    better menu

--- src/menu.py
+++ src/menu.py
@@ -2,167 +2,221 @@
 # Imports 
 import sys
 import os
+import time
 import matplotlib.pyplot as plt
 from pparser import write_data
+from tabulate import tabulate
 
 import algorithms as algo
+from algorithms import genetic_algorithm, tabu_search, get_sa_solution, hill_climbing_algorithm
 
+########################################
+# Global Libraries
+
+INPUT_FILES = {
+    '1': '../input/a_example.txt',
+    '2': '../input/b_read_on.txt',
+    '3': '../input/c_incunabula.txt',
+    '4': '../input/d_tough_choices.txt',
+    '5': '../input/e_so_many_books.txt',
+    '6': '../input/f_libraries_of_the_world.txt'
+}
+
+OUTPUT_FILES = {
+    '1': '../output/a_example.txt',
+    '2': '../output/b_read_on.txt',
+    '3': '../output/c_incunabula.txt',
+    '4': '../output/d_tough_choices.txt',
+    '5': '../output/e_so_many_books.txt',
+    '6': '../output/f_libraries_of_the_world.txt'
+}
+
+INITIAL_SOLUTIONS = {
+    '1': algo.generate_random_solution,
+    '2': "trivial_population",
+    '3': "greedy_population"
+    # add more
+}
+
+sa = "Simulated Annealing"
+ts = "Tabu Search"
+ga = "Genetic Algorithm"
+hc = "Hill Climbing Algorithm"
+
+ALGORITHMS =  {
+    '1': (algo.get_sa_solution, sa),
+    '2': (algo.tabu_search, ts),
+    '3': (algo.genetic_algorithm, ga),
+    '4': (algo.hill_climbing_algorithm, hc)
+    # add more
+}
+
+
+
+########################################
+# Auxiliar Functions
+
+# Clear screen
+def clear_screen():
+    os.system('clear')
 
 # Exit Menu Function
 def exit_application():
     print("Exiting the application. Goodbye!")
     sys.exit()
 
+# Input Handling Function
+def error_handling_input(input):
+    if input not in INPUT_FILES and input not in INITIAL_SOLUTIONS and input not in ALGORITHMS: 
+        print("Invalid choice. Please enter a valid option.")
+        input("Press Enter to continue...")
+        return book_scanning_menu()
 
-# Book Scanning Menu Function
+
+########################################
+def select_input_files():
+    clear_screen()
+    print("Please select an input file:")
+    for key, value in INPUT_FILES.items():
+        print(f"{key}. {value.split('/')[-1]}")
+    print("-------------------------------------------------------------")
+    choice = input("Please enter your choice: ")
+    return choice
+        
+
+def select_initial_population():
+    clear_screen()
+    print("Please select initial population:")
+    for key, value in INITIAL_SOLUTIONS.items():
+        print(f"{key}. {value}")
+    print("-------------------------------------------------------------")
+    choice = input("Please enter your choice: ")
+    return choice
+
+
+def select_algorithm():
+    clear_screen()
+    print("Please select an algorithm:")
+    for key, value in ALGORITHMS.items():
+        print(f"{key}. {value[1]}")  
+    print("-------------------------------------------------------------")
+    choice = input("Please enter your choice: ")
+    return choice
+
+
+
+def draw_graph(eval_scores):
+    x_positions = range(len(eval_scores))
+
+    # Plotting
+    plt.bar(x_positions, eval_scores, edgecolor='black')
+
+    # Set the title and labels
+    plt.title('Score Variation per Instance')
+    plt.xlabel('Instance')
+    plt.ylabel('Score Value')
+
+    # Set x-ticks to be the position of each score
+    plt.xticks(rotation=90)
+
+    # Optionally, adjust the y-axis to better show variations in scores
+    plt.ylim(min(eval_scores) - 10, max(eval_scores) + 10)  # Adjust as needed for visibility
+
+    plt.show()
+
+
 def book_scanning_menu():
     os.system('clear')
 
-    print("-------------------------------------------------------------")
-    print("| Book Scanning Menu                                        |")
-    print("-------------------------------------------------------------")
-    print("| Please select an input file :                             |")
-    print("| 1. a_example                                              |")
-    print("| 2. b_read_on                                              |")
-    print("| 3. c_incunabula                                           |")
-    print("| 4. d_tough_choices                                        |")
-    print("| 5. e_so_many_books                                        |")
-    print("| 6. f_libraries_of_the_world                               |")
-    print("| 0. Main Menu                                              |")
-    print("-------------------------------------------------------------")
-    inputfile = input("Please enter your choice: ")
+    input_file = select_input_files()
+    error_handling_input(input_file)
 
-    file_paths = {
-        '1': "../libraries/a_example.txt",
-        '2': "../libraries/b_read_on.txt",
-        '3': "../libraries/c_incunabula.txt",
-        '4': "../libraries/d_tough_choices.txt",
-        '5': "../libraries/e_so_many_books.txt",
-        '6': "../libraries/f_libraries_of_the_world.txt",
-        '0': "0"
-    }
+    init_solution = select_initial_population()
+    error_handling_input(init_solution)
 
-    if inputfile in file_paths:
-        print("-------------------------------------------------------------")
-        print("| Please select the initial solution:                       |")
-        print("| 1. Random Solution                                        |")
-        print("| 2. Trivial Solution                                       |")
-        print("| 3. Greedy Constrution                                     |")
-        print("| 0. Main Menu                                              |")
-        print("-------------------------------------------------------------")
-        init_solution = input("Please enter your choice: ")
+    selected_algorithm_key = select_algorithm()
+    error_handling_input(selected_algorithm_key)
 
-        sol_name = {
-        '1': algo.generate_random_solution,
-        '2': "Trivial Solution",
-        '3': "Greedy Constrution",
-        '0': "0"
-    }
-        sa = "Simulated Annealing"
-        ts = "Tabu Search"
-        ga = "Genetic Algorithm"
-        hc = "Hill Climbing Algorithm"
+    selected_algorithm = ALGORITHMS.get(selected_algorithm_key)
 
-        print("-------------------------------------------------------------")
-        print("| Please select the algorithm:                              |")
-        print("| 1. Simulated Annealing                                    |")
-        print("| 2. Tabu Search                                            |")
-        print("| 3. Genetic Algorithm                                      |")
-        print("| 4. Hill Climbing Algorithm                                |")
-        print("| 0. Main Menu                                              |")
-        print("-------------------------------------------------------------")
-        inputalgorithm = input("Please enter your choice: ")
+    start_time = time.time() # Start time 
 
-        options = {
-            '1': (algo.get_sa_solution, sa),
-            '2': (algo.tabu_search, ts),
-            '3': (algo.genetic_algorithm, ga),
-            '4': (algo.hill_climbing_algorithm, hc),
-            '0': menu
-        }
-
-        selected_algorithm = options.get(inputalgorithm)
-
-        if selected_algorithm:
-            best_solution , libraries_shipped,  eval_scores = selected_algorithm[0](file_paths[inputfile],sol_name[init_solution])  # Pass the file path directly
-            write_data(f'../libraries/{selected_algorithm[1]}.txt', best_solution, libraries_shipped)
-            choice = input("Select 0 for main menu or 1 to draw graph showing the algorithm evolution.\n")
-            if choice == '0':
-                menu()
-            elif choice == '1':
-
+    best_solution, libraries_shipped, eval_scores = selected_algorithm[0](INPUT_FILES[input_file], INITIAL_SOLUTIONS[init_solution])  
+    write_data(f'{OUTPUT_FILES[input_file]}', best_solution, libraries_shipped)
         
-                x_positions = range(len(eval_scores))
+    end_time = time.time() # End time
+    elapsed_time = end_time - start_time 
 
-                # Plotting
-                plt.bar(x_positions, eval_scores, edgecolor='black')
+    print_info(selected_algorithm, input_file, init_solution, eval_scores, elapsed_time)
 
-                # Set the title and labels
-                plt.title('Score Variation per Instance')
-                plt.xlabel('Instance')
-                plt.ylabel('Score Value')
-
-                # Set x-ticks to be the position of each score
-                plt.xticks(rotation=90)
-
-                # Optionally, adjust the y-axis to better show variations in scores
-                plt.ylim(min(eval_scores) - 10, max(eval_scores) + 10)  # Adjust as needed for visibility
-
-                plt.show()
-
-                choice = input("Select 0 for main menu.\n")
-                if choice == '0':
-                    menu()
-
-            else:
-                print("Invalid choice. Please enter a valid option.")
-                best_score_menu()
-        else:
-            print("Invalid choice. Please enter a valid option.")
-            best_scanning_menu()
-    elif inputfile == '0':
+    choice = input("Press 1 for showing the graph of the algorithm; Press 0 for main menu.\n")
+    if choice == '0':
         menu()
     else:
-        print("Invalid choice. Please enter a valid option.")
-        best_scanning_menu()
+        draw_graph(eval_scores)
+        input("Press Enter to continue...")
+    menu()
 
+
+
+# Helper function to print algorithm information
+def print_info(alg_name, file_path, type_initial_population, eval_scores, time_taken):
+    clear_screen()
+    print("-------------------------------------------------------------")
+    print(f"Algorithm: {alg_name}")
+    print(f"File: {file_path}")
+    print(f"Initial Solution: {type_initial_population}")
+    print(f"The final score was: {eval_scores}")
+    print(f"Time taken: {time_taken:.6f} seconds")
+    print("-------------------------------------------------------------")
+
+
+# Get and Print Content of Best Score Menu Function
+def get_content_best_score():
+    with open('best_score.txt', 'r') as f:
+        lines = f.readlines()
+
+    # Initialize table headers and rows
+    headers = ['File', 'Initial Solution', 'Algorithm', 'Score', 'Time']
+    rows = []
+
+    # Skip the first line (header)
+    lines = lines[1:]
+
+    # Parse each line and extract relevant information
+    for line in lines:
+        file_name, initial_solution, algorithm, score, time = line.strip().split(',')
+        rows.append([file_name.strip(), initial_solution.strip(), algorithm.strip(), int(score), float(time)])
+
+    return tabulate(rows, headers=headers)
 
 
 # Best Score Menu Function
 def best_score_menu():
-    os.system('clear')
+    clear_screen()
 
+   # Print Init UI
     print("--------------------------------------------------------------------------------")
-    print("| Best Score Menu                                                              |")
+    print("Best Score Menu                                                              ")
     print("--------------------------------------------------------------------------------")
-    print("| File                      | Score       | Initial Solution | Algorithm       |")
-    print("| a_example                 | 000 000 000 | -                | -               |")
-    print("| b_read_on                 | 000 000 000 | -                | -               |")
-    print("| c_incunabula              | 000 000 000 | -                | -               |")
-    print("| d_tough_choices           | 000 000 000 | -                | -               |")
-    print("| e_so_many_books           | 000 000 000 | -                | -               |")
-    print("| f_libraries_of_the_world  | 000 000 000 | -                | -               |")
+    print(get_content_best_score())
     print("--------------------------------------------------------------------------------")
-
-    choice = input("Press 0 to go back to main menu.\n")
-    if choice == '0':
-        menu()
-    else:
-        print("Invalid choice. Please enter a valid option.")
-        best_score_menu()
+    input("Press Enter to continue...")
+    menu()
 
 
 # Main menu Function
 def menu():
-    os.system('clear')
+    clear_screen()
 
+   # Print Init UI
     print("-------------------------------------------------------------")
-    print("| Welcome to our application                                |")
+    print("Welcome to our application")
     print("-------------------------------------------------------------")
-    print("| What do you want to do:                                   |")
-    print("| 1. Book Scanning                                          |")
-    print("| 2. See the best score for each library                    |")
-    print("| 0. Exit                                                   |")
+    print("What do you want to do:")
+    print("1. Book Scanning")
+    print("2. See the best score for each library")
+    print("0. Exit")
     print("-------------------------------------------------------------")
     
     choice = input("Please enter your choice: ")
@@ -179,4 +233,5 @@ def menu():
         selected_option()
     else:
         print("Invalid choice. Please enter a valid option.")
+        input("Press Enter to continue...")
         menu()
